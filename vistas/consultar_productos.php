9ae53bad9aa3b18376e4776328113d31ae916e43
miguelangelborbonclavijo <105363180+miguelangelborbonclavijo@users.noreply.github.com>
2023-06-15 07:37:52 -0500

    subir proyecto
    Subir el proyecto por primera vez

---
+++ vistas/consultar_productos.php
@@ -0,0 +1,39 @@
+<?php include("../includes/head.php") ?>
+<main style="margin-top:30px;">
+<div class="container-lg">
+<table class="table">
+<thead>
+<tr>
+<h1 style="text-align: center;">PRODUCTOS REGISTRADOS</h1>
+<br>
+</tr>
+<tr style="text-align: center;">
+<th scope="col">TIPO DE PRODUCTO</th>
+<th scope="col">DESCRIPCION</th>
+<th scope="col">PRECIO DE COMPRA</th>
+<th scope="col">PRECIO DE VENTA</th>
+<th scope="col">ESTADOS</th>
+</tr>
+</thead>
+<tbody>
+<?php
+$query = "SELECT * FROM productos";
+$resulproductos = mysqli_query($conn, $query);
+while($filas = mysqli_fetch_array($resulproductos)){?>
+<tr style="text-align: center;">
+<td><?php echo $filas['ProTip'] ?></td>
+<td><?php echo $filas['ProDes'] ?></td>
+<td><?php echo $filas['ProPreCo'] ?></td>
+<td><?php echo $filas['ProPreVe'] ?></td>
+<td>
+<a href="editar_productos.php?id=<?php echo $filas['ProId']?>">Editar</a>
+<a href="eliminar_productos.php?id=<?php echo $filas['ProId']?>">Eliminar</a>
+</td>
+</tr>
+<?php } ?> 
+</tbody> 
+</tbody>
+</table>
+</div>
+</main>
+<?php include("../includes/footer.php") ?>
---
+++ vistas/registros/eliminar_personas.php
@@ -0,0 +1,12 @@
+<?php
+    include('db.php');
+    if(isset($_GET['id'])){
+        $id = $_GET['id'];
+        $query = "DELETE FROM personas WHERE PerId = $id";
+        $resuleliminar = mysqli_query($conn, $query);
+        if (!$resuleliminar){
+            die ("Error eliminar");
+        }
+        header("Location: ../registros/consultar_personas.php"); 
+    }
+?>|
